95a3567d8ba2fd842f9c3c32602866d05c76c7ae
Thiago <Thiago@192.168.1.11>
2021-12-18 11:21:50 -0200

    Criação do Projeto

---
+++ primeiro-projeto-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,12 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+
+	/*Mains é o metodo auto executavel em Java*/
+	public static void main(String[] args) {
+		
+		/*Escreve o texto no console*/
+		System.out.println("Meu primeiro programa em Java - do Curso Formalção Java Web");
+		System.out.println("Java é orientado a objetos");
+	}
+}
